7eb8490ea21a9e592c9e4f3c40873ad86d9378b2
Rola Alnabulsi <rola.nabulsi@gmail.com>
2023-03-11 21:21:47 +0200

    Fianl Project

--- erpnext_gsg/erpnext_gsg/doc_events/excuse_application.py
+++ erpnext_gsg/erpnext_gsg/doc_events/excuse_application.py
@@ -6,14 +6,16 @@ def validate(doc,method):
     if doc.from_time > doc.to_time:
         frappe.throw("To Time cannot be before from Time")
 
-    doc.new_excuse_hours = doc.excuse_hours_alowed
+    # doc.new_excuse_hours = doc.excuse_hours_alowed
     # set_value = frappe.db.set_value("Employee Excuse application",doc.new_excuse_hours,str(doc.excuse_hours_alowed))
     # doc.new_excuse_hours = set_value
     get_hours = time_diff_in_hours(doc.to_time, doc.from_time)
     doc.hours = get_hours
 
-    after_excuse = doc.new_excuse_hours - doc.hours
-    frappe.db.set_value("Employee Excuse application",doc.employee_name,doc.new_excuse_hours,after_excuse)
+    after_excuse = doc.excuse_hours_alowed - doc.hours
+    doc.new_excuse_hours = after_excuse
+    # doc.excuse_hours_alowed = after_excuse
+    # frappe.db.set_value("Employee Excuse application",doc.employee_name,doc.excuse_hours_alowed,after_excuse)
     if not doc.excuse_hours_alowed:
         frappe.throw("There is No  Excuse Hours available in your department")
 
